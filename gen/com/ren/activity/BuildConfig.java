33afd5f72fe652b5ec489792ffc2929c2102a65d
韩亚飞_韩梦飞沙 <313134555@qq.com>
2017-09-15 12:05:57 +0800

    0

---
+++ gen/com/ren/activity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ren.activity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ren/methed/Track_Methed.java
@@ -0,0 +1,29 @@
+package com.ren.methed;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+public class Track_Methed implements Runnable {
+
+	// 播放暂存
+	int out_size;
+	// 放音对象
+	AudioTrack track = null;
+
+	public void run() {
+
+	}
+
+	// 播送方法
+	public void track() {
+		out_size = android.media.AudioTrack.getMinBufferSize(8000,
+				AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
+
+		track = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
+				AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_16BIT, out_size,
+				AudioTrack.MODE_STREAM);
+	}
+}
